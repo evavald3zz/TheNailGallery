9656895ecc94b4fcdf3aee39e5be3ed1cc3cfc14
evavald3zz <3vavaldez@gmail.com>
2025-02-25 20:21:56 -0500

    Initial Commit

---
+++ TheNailGallery/TheNailGalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheNailGalleryApp.swift
+//  TheNailGallery
+//
+//  Created by M1_Tugo on 2/25/25.
+//
+
+import SwiftUI
+
+@main
+struct TheNailGalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TheNailGalleryTests/TheNailGalleryTests.swift
@@ -0,0 +1,17 @@
+//
+//  TheNailGalleryTests.swift
+//  TheNailGalleryTests
+//
+//  Created by M1_Tugo on 2/25/25.
+//
+
+import Testing
+@testable import TheNailGallery
+
+struct TheNailGalleryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
